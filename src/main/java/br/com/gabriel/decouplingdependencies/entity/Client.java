895edb167b9d97fbf1ee4b619fb31af8520b1a6c
da0hn <gabrielhondacba@gmail.com>
2022-07-09 11:35:13 -0400

    feat: add `ClientRepository` and `spring-data` annotations

--- src/main/java/br/com/gabriel/decouplingdependencies/entity/Client.java
+++ src/main/java/br/com/gabriel/decouplingdependencies/entity/Client.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 
@@ -13,12 +20,17 @@ import java.time.LocalDate;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "clients")
 public class Client {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
 
+  @Enumerated(EnumType.STRING)
   private Gender gender;
 
   private String user;
