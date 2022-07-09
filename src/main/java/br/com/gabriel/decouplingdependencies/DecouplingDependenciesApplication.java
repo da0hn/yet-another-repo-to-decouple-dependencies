701e87a08aa3a0ba742945a1e36f18cd71517ab8
da0hn <gabrielhondacba@gmail.com>
2022-07-09 10:31:19 -0400

    chore: initial commit 🎉

---
+++ src/main/java/br/com/gabriel/decouplingdependencies/DecouplingDependenciesApplication.java
@@ -0,0 +1,14 @@
+package br.com.gabriel.decouplingdependencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DecouplingDependenciesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DecouplingDependenciesApplication.class, args);
+  }
+
+}
