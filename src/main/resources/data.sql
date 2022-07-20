bfd26de683816a0796f223db6613f7e3b7138a02
da0hn <gabrielhondacba@gmail.com>
2022-07-19 20:07:23 -0400

    chore: add initial dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO cadastro_entrega.clients (cep, birth_date, gender, name, password, user)
+VALUES ('01310200', '1994-07-14', 'MALE', 'gabriel', '123456', 'gabriel');
+INSERT INTO cadastro_entrega.clients (cep, birth_date, gender, name, password, user)
+VALUES ('78051408', '1993-07-20', 'MALE', 'Leonard', '$z6c92xFTIvrRdhH', 'Kailey16');
+INSERT INTO cadastro_entrega.clients (cep, birth_date, gender, name, password, user)
+VALUES ('41321205', '1993-04-19', 'MALE', 'Joey', '$sDWitqQEF4Yhjfo', 'Gilberto_Pagac');
+INSERT INTO cadastro_entrega.clients (cep, birth_date, gender, name, password, user)
+VALUES ('08090284', '1991-01-31', 'FEMALE', 'Arvid', 'bbrNDn3PLoliV9R', 'Jazlyn_Parker');
