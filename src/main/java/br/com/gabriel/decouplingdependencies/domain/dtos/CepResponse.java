56b58aa13943b9ace5e70fa6fdb5eb31aebb3a80
Da0hn <gabrielhondacba@gmail.com>
2022-09-25 22:14:51 -0400

    refactor: remove builder implementation

--- src/main/java/br/com/gabriel/decouplingdependencies/domain/dtos/CepResponse.java
+++ src/main/java/br/com/gabriel/decouplingdependencies/domain/dtos/CepResponse.java
@@ -9,58 +9,8 @@ public record CepResponse(
   String uf
 ) implements FetchCepResponse {
 
-
-  public static Builder newBuilder() {
-    return new Builder();
-  }
-
   public boolean realizaEntrega() {
     return "SP".equalsIgnoreCase(this.localidade) || "RJ".equalsIgnoreCase(this.localidade);
-  }
-
-  public static final class Builder {
-
-    private String logradouro;
-
-    private String bairro;
-
-    private String localidade;
-
-    private String uf;
-
-    private Builder() {
-    }
-
-    public Builder withLogradouro(final String val) {
-      this.logradouro = val;
-      return this;
-    }
-
-    public Builder withBairro(final String val) {
-      this.bairro = val;
-      return this;
-    }
-
-    public Builder withLocalidade(final String val) {
-      this.localidade = val;
-      return this;
-    }
-
-    public Builder withUf(final String val) {
-      this.uf = val;
-      return this;
-    }
-
-
-    public CepResponse build() {
-      return new CepResponse(
-        this.logradouro,
-        this.bairro,
-        this.localidade,
-        this.uf
-      );
-    }
-
   }
 
 }
