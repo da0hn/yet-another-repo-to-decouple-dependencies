75555fd97d39ef9f441805e7658c391771a8748b
Da0hn <gabrielhondacba@gmail.com>
2022-09-25 22:53:29 -0400

    chore: seed initial data of database using `hibernate`

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- @formatter:off
+INSERT INTO clients VALUES (1, '01310200', '1994-07-14', 'MALE', 'gabriel', '123456', 'gabriel');
+INSERT INTO clients VALUES (2, '78051408', '1993-07-20', 'MALE', 'Leonard', '$z6c92xFTIvrRdhH', 'Kailey16');
+INSERT INTO clients VALUES (3, '41321205', '1993-04-19', 'MALE', 'Joey', '$sDWitqQEF4Yhjfo', 'Gilberto_Pagac');
+INSERT INTO clients VALUES (4, '08090284', '1991-01-31', 'FEMALE', 'Arvid', 'bbrNDn3PLoliV9R', 'Jazlyn_Parker');
